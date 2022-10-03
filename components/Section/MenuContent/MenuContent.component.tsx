bedfbacc31cc6c7be0e11cdda25da6cd5b561c40
Jakub Lobos <saltver2@onet.eu>
2022-10-03 23:12:27 +0200

    CMS not working

--- components/Section/MenuContent/MenuContent.component.tsx
+++ components/Section/MenuContent/MenuContent.component.tsx
@@ -1,11 +1,12 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Category } from "./Category/Category.component";
 import { MenuContentWrapper } from "./MenuContent.style";
 import { ProductRow } from "./Category/ProductRow/Price/ProductRow.component";
 import { Ingredient } from "./Ingredient/Ingredinent.style";
 import { GraphQLClient, gql } from "graphql-request";
+import React from "react";
 
-const sushiCMS:GraphQLClient = new GraphQLClient("https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cl8nnv5lx05qd01tcef9ggegp/master")
+
 
 const QUERY:string = gql`
     query MyQuery {
@@ -17,33 +18,56 @@ const QUERY:string = gql`
     }
 `
 
-export async function getContentMenuProps() {
-    const menuCategories = await sushiCMS.request(QUERY); //get request from api and require content from QUERY
+export const MenuContent: FC = () => {
     
-    return {
-        props: {menuCategories}, //define props
-        revalidate: 10, // refreshing content
-    };
-};
+    const sushiCMS:GraphQLClient = new GraphQLClient("https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cl8nnv5lx05qd01tcef9ggegp/master")
+    const [menuCategories, setMenuCategories]:Array<any> = useState([]);
 
-interface MenuContentProp {
-    menuCategories: Array<any>,
- }
+    useEffect(() => {
+        async function getStaticProps() {
+            const menuCategoriesArr: any = await sushiCMS.request(QUERY); //get request from api and require content from QUERY
+            setMenuCategories(menu => [...menu, menuCategoriesArr]);
+            console.log(menuCategoriesArr)
+            console.log(menuCategories)
+            return {
+                props: { menuCategories }, //define props
+                revalidate: 10, // refreshing content
+            };
+        };
+        getStaticProps();
+    }, []);
 
-export const MenuContent: FC<MenuContentProp> = ( {menuCategories} ) => {
+    // useEffect(() => {
+    //     async function getStaticProps() {
+    //         try {
+    //             const menuCategoriesArr: any = await sushiCMS.request(QUERY);
+    //             menuCategoriesArr.then((result: any) => {
+    //               console.log(result)
+    //               if (result) {
+    //                 return setMenuCategories(result);
+    //               } else { 
+    //                 console.log(result)
+    //               }
+    //             });
+    //             } catch (err) {
+    //               console.log(err)
+    //             }
+    //     };
+    //     getStaticProps();
+    // }, []);
+
+
     return (
         <MenuContentWrapper>
             {
-                menuCategories?.map((category) => (
+                menuCategories.map((category) => (
                     <Category categoryHeading={category.categoryName}>
                     </Category>
                 ))
-                
             }
         </MenuContentWrapper>
     )
 };
-
 
 
 
