bd704c3a1f3c7db36575fe1b0c91cd94c77cf143
Jakub Lobos <saltver2@onet.eu>
2022-10-05 21:36:51 +0200

    Cms fixed

--- components/Section/MenuContent/Category/ProductRow/Price/ProductRow.component.tsx
+++ components/Section/MenuContent/Category/ProductRow/Price/ProductRow.component.tsx
@@ -4,16 +4,17 @@ import { StyledProductRow } from "../ProductRow.style";
 
 interface ProductProperties {
     children: ReactElement[];
-    price: string;
-    amount: string;
+    price: number;
+    amount: number;
+    productName: string;
 }
 
 export const ProductRow:FC<ProductProperties> = (props:PropsWithChildren<ProductProperties>) => {
     return (
         <StyledProductRow>
-            <h3>Futomaki</h3>
+            <h3>{props.productName}</h3>
             <ul>{props.children}</ul>
-            <Price>{props.price}</Price>
+            <Price>{props.price}$</Price>
             <p>{props.amount}pcs.</p>
         </StyledProductRow>
     )
--- components/Section/MenuContent/MenuContent.component.tsx
+++ components/Section/MenuContent/MenuContent.component.tsx
@@ -6,29 +6,42 @@ import { Ingredient } from "./Ingredient/Ingredinent.style";
 import { GraphQLClient, gql } from "graphql-request";
 import React from "react";
 
-
-
 const QUERY:string = gql`
-    query MyQuery {
-        menuCategories {
-        categoryName
-        id
-        slug
+query MyQuery {
+    menuCategories {
+      categoryName
+      id
+      slug
+      products {
+        ... on ProductRow {
+          id
+          amount
+          productName
+          price
+          ingredients {
+            ... on Ingredient {
+              id
+              nameOfIngredient
+            }
+          }
         }
+      }
     }
+  }
+  
 `
 
 export const MenuContent: FC = () => {
     
     const sushiCMS:GraphQLClient = new GraphQLClient("https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cl8nnv5lx05qd01tcef9ggegp/master")
-    const [menuCategories, setMenuCategories]:Array<any> = useState([]);
+    const [menuCategories, setMenuCategories]: Array<any> = useState([]);
+
 
     useEffect(() => {
         async function getStaticProps() {
             const menuCategoriesArr: any = await sushiCMS.request(QUERY); //get request from api and require content from QUERY
             setMenuCategories(menuCategoriesArr.menuCategories);
-            console.log(menuCategoriesArr)
-
+            console.log(menuCategories)
             return {
                 props: { menuCategories }, //define props
                 revalidate: 10, // refreshing content
@@ -37,30 +50,31 @@ export const MenuContent: FC = () => {
         getStaticProps();
     }, []);
 
-    // useEffect(() => {
-    //     async function getStaticProps() {
-    //         try {
-    //             const menuCategoriesArr: any = await sushiCMS.request(QUERY);
-    //             menuCategoriesArr.then((result: any) => {
-    //               console.log(result)
-    //               if (result) {
-    //                 return setMenuCategories(result);
-    //               } else { 
-    //                 console.log(result)
-    //               }
-    //             });
-    //             } catch (err) {
-    //               console.log(err)
-    //             }
-    //     };
-    //     getStaticProps();
-    // }, []);
-
     return (
         <MenuContentWrapper>
             {
-                menuCategories?.map((category) => (
-                    <Category categoryHeading={category.categoryName}>
+                menuCategories.map((category) => (
+                    <Category
+                        categoryHeading={category.categoryName}
+                        key={category.id}>
+                        {
+                            category.products.map((product) => (
+                                <ProductRow
+                                    productName={product.productName}
+                                    price={product.price}
+                                    amount={product.amount}
+                                    key={product.id}>
+                                    {
+                                        product.ingredients.map((ingredient) => (
+                                            <Ingredient
+                                                key={ingredient.id}>
+                                                {ingredient.nameOfIngredient}
+                                            </Ingredient>
+                                        ))
+                                    }
+                                </ProductRow>
+                            ))
+                        }
                     </Category>
                 ))
             }
